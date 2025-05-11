9705406ad1b7e2408fd9477a5072ec8cda105ec4
Abhishek <punhani.manavabhi@gmail.com>
2025-05-11 20:37:40 +0530

    added theme provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,19 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { ThemeProvider } from "@/contexts/ThemeProvider";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+export default function App({
+  Component,
+  pageProps: { session, ...pageProps },
+}: AppProps) {
+  return (
+    <ThemeProvider
+      attribute="class"
+      defaultTheme="system"
+      enableSystem
+      disableTransitionOnChange
+    >
+        <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
