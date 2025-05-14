efad298ea9d4a379301856a294cb999f92f1cd57
RashiJyotishi <jyotishirashi1610@gmail.com>
2025-05-14 15:18:10 +0530

    limbo logic improved

--- components/limbo/LimboGame.tsx
+++ components/limbo/LimboGame.tsx
@@ -43,19 +43,20 @@ const LimboGame: React.FC<LimboGameProps> = ({ balance, onBalanceChange, onBetCo
   };
 
   const generateRandomMultiplier = () => {
-  const lowerBound = 0.5 * targetMultiplier;
-  let randFactor: number;
-  if (targetMultiplier <= 30) {
+    const lowerBound = 0.5 * targetMultiplier;
+    let randFactor: number;
+    if (targetMultiplier <= 30) {
 
-    randFactor = Math.random() * (2 - 0.5) + 0.5;
-  } else {
+      randFactor = Math.random() * (2 - 0.5) + 0.5;
+    } else {
 
-    randFactor = Math.random() * (1.5 - 0.5) + 0.5;
-  }
-  const X = targetMultiplier * randFactor;
-  const result = Math.random() * (X - lowerBound) + lowerBound;
-  return result;
-};
+      randFactor = Math.random() * (1.5 - 0.5) + 0.5;
+    }
+    const X = targetMultiplier * randFactor;
+
+    const result = Math.random() * (X - lowerBound) + lowerBound;
+    return result;
+  };
 
   const handlePlay = () => {
     if (betAmount <= 0) {
